403a8dbe8ef84401debd3cdf53193d9cde59ae10
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-08 16:53:31 +0200

    Fixed icons

--- src/components/FavoriteCompanies.jsx
+++ src/components/FavoriteCompanies.jsx
@@ -1,6 +1,7 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import Job from "./Job";
+import { NavLink } from "react-router-dom";
 
 const FavoriteCompanies = () => {
   const favoriteCompanies = useSelector(state => state.favorites.content);
@@ -10,12 +11,22 @@ const FavoriteCompanies = () => {
         <Col xs={10} className="mx-auto my-3">
           <h1 className="display-1">Favorite Companies</h1>
         </Col>
-
-        {favoriteCompanies.map(company => (
-          <Col key={company._id} xs="10" className="mx-auto">
-            <Job data={company} />
+        {favoriteCompanies.length < 1 ? (
+          <Col xs="10" className="mx-auto">
+            <h2 className="lead">No favorites, add some!</h2>
           </Col>
-        ))}
+        ) : (
+          favoriteCompanies.map(company => (
+            <Col key={company._id} xs="10" className="mx-auto">
+              <Job data={company} />
+            </Col>
+          ))
+        )}
+        <Col xs="10" className="mx-auto">
+          <NavLink to="/" className="btn btn-primary my-3">
+            Go Back
+          </NavLink>
+        </Col>
       </Row>
     </Container>
   );
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -11,12 +11,20 @@ const Job = ({ data }) => {
   return (
     <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
       <Col xs={3}>
-        <div className="d-flex gap-2">
+        <div className="d-flex gap-2 align-items-center">
           <Link to={`/${data.company_name}`}>{data.company_name}</Link>
           {location.pathname !== "/favorites" ? (
-            <Star type="button" onClick={() => addToFavorites({ type: "ADD_TO_FAVORITES", payload: data })} />
+            <Star
+              type="button"
+              className=" flex-shrink-0"
+              onClick={() => addToFavorites({ type: "ADD_TO_FAVORITES", payload: data })}
+            />
           ) : (
-            <Trash type="button" onClick={() => removeFavorite({ type: "REMOVE_FROM_FAVORITES", payload: data._id })} />
+            <Trash
+              type="button"
+              className="flex-shrink-0"
+              onClick={() => removeFavorite({ type: "REMOVE_FROM_FAVORITES", payload: data._id })}
+            />
           )}
         </div>
       </Col>
